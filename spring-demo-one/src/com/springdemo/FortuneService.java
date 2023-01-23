dfd7ca07d4ca1e5e60c2f6376a4957d3e4fa7cb0
Achraf AISSY <105090484+AissyAchraf@users.noreply.github.com>
2023-01-23 13:33:53 +0100

    Injection des dépendances : Activité
    1. Define a new implementation for the FortuneService.
    
         a. When the getFortune() method is called it should return a random fortune from the array.
    
         b. Your fortune service should define three fortunes in an array.
    
    2. Inject your new dependency into your Coach implementation.
    
    3. Test your application to verify you are retrieving random fortunes.

--- spring-demo-one/src/com/springdemo/FortuneService.java
+++ spring-demo-one/src/com/springdemo/FortuneService.java
@@ -1,6 +1,9 @@
 package com.springdemo;
 
+
 public interface FortuneService {
+	
+	String[] Fortunes = {"10000", "11000", "8000"};
 	
 	public String getFortune();
 
--- spring-demo-one/src/com/springdemo/tennisCoach.java
+++ spring-demo-one/src/com/springdemo/tennisCoach.java
@@ -1,6 +1,38 @@
 package com.springdemo;
 
 public class tennisCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	private String emailAddresse;
+	private String team;
+	
+	public tennisCoach() {
+		System.out.println("tennisCoach : inside no-arg constructor");
+	}
+
+	public String getEmailAddresse() {
+		return emailAddresse;
+	}
+
+	public void setEmailAddresse(String emailAddresse) {
+		System.out.println("tennisCoach : inside setter setEmailAddresse");
+		this.emailAddresse = emailAddresse;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("tennisCoach : inside setter setTeam");
+		this.team = team;
+	}
+
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("tennisCoach : inside setter setFortuneService");
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -9,7 +41,7 @@ public class tennisCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
